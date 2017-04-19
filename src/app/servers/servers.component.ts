21a3035e15ad59ea18342215254b0fb82d2d79bf
David ZAOUI <david.zaoui@afnor.org>
2017-04-19 18:09:49 +0200

    directives

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'TestServer';
+  serverCreated = false;
+
+  constructor() { 
+    setTimeout(() => {this.allowNewServer = true }, 2000);
+  }
+
+  ngOnInit() {
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+    this.serverCreated = true;
+  }
+
+  onUpdateServerName(event : any){
+    this.serverName = event.target.value;
+
+  }
+
+}
